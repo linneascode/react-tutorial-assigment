57c6a2037e110c198928bd11ebc28fa959454a67
Linnea Åkesson <linnea.akesson@hyperisland.se>
2023-12-22 10:04:21 +0100

    React tutorial assigment

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Alert from "./components/alert";
+import Button from "./components/button";
+
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
